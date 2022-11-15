2183ffcafed2c37ed5b67717f95d1646be541ac7
Jakub Podoba <51724203+jpodoba@users.noreply.github.com>
2022-11-15 15:06:11 +0100

    Update mongodb-update-bindip.ps1

--- mongodb-update-bindip.ps1
+++ mongodb-update-bindip.ps1
@@ -1,4 +1,4 @@
 $rootPath = "C:\Program Files\MongoDB"
 $cfgPath = Get-ChildItem -Recurse -Filter "mongod.cfg" -path $rootPath
 $ip = Get-NetIPAddress -PrefixOrigin Dhcp -AddressFamily IPv4 | select -ExpandProperty IPAddress
-(Get-Content $cfgPath.FullName) -Replace '127.0.0.1', $ip | Set-Content ($cfgPath.FullName + "bak") 
+(Get-Content $cfgPath.FullName) -Replace '127.0.0.1', $ip | Set-Content $cfgPath.FullName 
